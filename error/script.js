cc4a9d58ce1745334801cb06131eed9f11e1e5af
aleefarizwana <126440963+aleefarizwana@users.noreply.github.com>
2023-07-31 21:07:13 +0530

    script.js
    java script file consists of alert message.to display alert message ,html page must be attached using script tag .

---
+++ error/script.js
@@ -0,0 +1,13 @@
+var num =1;
+console.log(num);
+
+alert("This is warning.....")
+
+
+//output
+// alert("This is warning.....")
+// ^
+
+// ReferenceError: alert is not defined
+//if we run in terminal 
+//so create html file and run in browser
